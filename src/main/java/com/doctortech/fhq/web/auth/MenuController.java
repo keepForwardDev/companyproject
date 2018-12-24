d801a4698113b8c091b3a2a421aeb2eec905cd6b
jaxMine <304872337@qq.com>
2018-12-25 00:24:30 +0800

    --

--- src/main/java/com/doctortech/fhq/web/auth/MenuController.java
+++ src/main/java/com/doctortech/fhq/web/auth/MenuController.java
@@ -1,8 +1,10 @@
 package com.doctortech.fhq.web.auth;
 
+import com.doctortech.fhq.service.common.MenuService;
 import com.doctortech.fhq.web.BaseController;
 import com.doctortech.framework.bean.CommonRespon;
 import com.doctortech.framework.consts.Const;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MenuController extends BaseController{
 
+    @Autowired
+    private MenuService menuService;
     /**
      * 登陆用户菜单
      * @return
@@ -23,6 +27,14 @@ public class MenuController extends BaseController{
         res.setCode(Const.CODE_SUCCESS);
         res.setMsg(Const.CODE_SUCCESS_STR);
         res.setData(getCurrentUser().getMenu());
+        return res;
+    }
+
+    @RequestMapping(value = "/menuList",method = RequestMethod.GET)
+    @ResponseBody
+    public CommonRespon menuList(String title) {
+        CommonRespon res= success();
+        res.setData(menuService.getMenuList(title));
         return res;
     }
 }
