8d1660b4551a721f006c5fcd675c81719ffd9875
jaxMine <304872337@qq.com>
2018-12-30 03:57:57 +0800

    --tree

--- src/main/java/com/doctortech/fhq/web/auth/ResourcesController.java
+++ src/main/java/com/doctortech/fhq/web/auth/ResourcesController.java
@@ -2,12 +2,14 @@ package com.doctortech.fhq.web.auth;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.doctortech.fhq.bean.MenuResources;
-import com.doctortech.fhq.bean.Router;
+import com.doctortech.fhq.bean.ResourcesBean;
 import com.doctortech.fhq.service.common.MenuService;
+import com.doctortech.fhq.service.common.ResourcesService;
 import com.doctortech.fhq.web.BaseController;
 import com.doctortech.framework.bean.CommonRespon;
 import com.doctortech.framework.consts.Const;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
@@ -19,60 +21,52 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.validation.Valid;
 import java.util.List;
 
-@RequestMapping("/menu")
+@RequestMapping("resource")
 @Controller
-public class MenuController extends BaseController{
+public class ResourcesController extends BaseController{
 
     @Autowired
     private MenuService menuService;
-    /**
-     * 登陆用户菜单
-     * @return
-     */
-    @RequestMapping(value = "/me",method = RequestMethod.GET)
-    @ResponseBody
-    public CommonRespon myMenu() {
-        CommonRespon res= new CommonRespon();
-        res.setCode(Const.CODE_SUCCESS);
-        res.setMsg(Const.CODE_SUCCESS_STR);
-        res.setData(getCurrentUser().getMenu());
-        return res;
-    }
 
-    @RequestMapping(value = "/menuList",method = RequestMethod.GET)
-    @ResponseBody
-    public CommonRespon menuList(String title) {
-        CommonRespon res= success();
-        res.setData(menuService.getMenuList(title));
-        return res;
-    }
+    @Autowired
+    private ResourcesService resourcesService;
 
-    @RequestMapping(value = "/menuTree",method = RequestMethod.GET)
+    @RequestMapping(value = "/menuResources",method = RequestMethod.POST)
     @ResponseBody
-    public CommonRespon menuTree() {
+    public CommonRespon getMenuResources(MenuResources rs, Page<MenuResources> page) {
         CommonRespon res= success();
-        res.setData(menuService.getMenuTree());
+        res.setData(menuService.getMenuResources(rs,page));
         return res;
     }
 
     @RequestMapping(value = "/save",method = RequestMethod.POST)
     @ResponseBody
-    public CommonRespon saveMenu(@RequestBody @Valid Router r, BindingResult result) {
-        CommonRespon res= new CommonRespon();
+    public CommonRespon saveOrUpdate(@RequestBody @Valid ResourcesBean bean, BindingResult result) {
+        CommonRespon res= success();
         List<ObjectError> error = result.getAllErrors();
         if (error.size() > 0) {
             res.setCode(Const.CODE_ERROR);
             res.setMsg(error.get(0).getDefaultMessage());
             return res;
         }
-        menuService.saveOrUpdateMenu(r);
-        return success();
+        try {
+            resourcesService.saveOrUpdate(bean);
+        } catch (DuplicateKeyException e) {
+            res.setCode(Const.CODE_ERROR);
+            res.setMsg("该资源编码已存在，请重新输入！");
+        }
+        return res;
     }
+
 
     @RequestMapping(value = "/delete",method = RequestMethod.GET)
     @ResponseBody
-    public CommonRespon deleteMenu(Long id) {
-        menuService.delete(id);
-        return success();
+    public CommonRespon delete(String id) {
+        if (id==null) {
+            return faild();
+        }
+        CommonRespon res= success();
+        resourcesService.delete(id);
+        return res;
     }
 }
