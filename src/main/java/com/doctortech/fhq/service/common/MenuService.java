a9734ead8a43128d8a5e30600689d89034249680
jaxMine <304872337@qq.com>
2019-01-02 23:57:34 +0800

    --tree

--- src/main/java/com/doctortech/fhq/service/common/MenuService.java
+++ src/main/java/com/doctortech/fhq/service/common/MenuService.java
@@ -32,8 +32,10 @@ public class MenuService {
 
     @Autowired
     private ResourceMapper resourceMapper;
+
     /**
      * 获取用户菜单
+     *
      * @param rolesId
      * @return
      */
@@ -46,27 +48,28 @@ public class MenuService {
 
     /**
      * 获取用户路由
+     *
      * @param rolesId
      * @return
      */
     public List<Router> getUserRouters(List<Long> rolesId) {
-        List<Menu> menu= selectUserMenus(rolesId);
-        List<Router> routers= new ArrayList<>();
+        List<Menu> menu = selectUserMenus(rolesId);
+        List<Router> routers = new ArrayList<>();
         //根节点
-        List<Menu> parents= menu.stream().filter(m->m.getParentId()==null).collect(Collectors.toList());
+        List<Menu> parents = menu.stream().filter(m -> m.getParentId() == null).collect(Collectors.toList());
         //子节点
-        List<Menu> child= menu.stream().filter(m->m.getParentId()!=null).collect(Collectors.toList());
+        List<Menu> child = menu.stream().filter(m -> m.getParentId() != null).collect(Collectors.toList());
         if (parents.isEmpty()) {
             return routers;
         }
-        parents.forEach(p->{
-            routers.add(getRouter(p,child));
+        parents.forEach(p -> {
+            routers.add(getRouter(p, child));
         });
         return routers;
     }
 
-    private Router getRouter(Menu parent ,List<Menu> all) {
-        Router router= new Router();
+    private Router getRouter(Menu parent, List<Menu> all) {
+        Router router = new Router();
         router.setPath(parent.getPath());
         router.setComponent(parent.getComponent());
         router.setMeta(JSONObject.parseObject(parent.getMeta()));
@@ -76,9 +79,9 @@ public class MenuService {
         router.setParentId(parent.getParentId());
         router.setSort(parent.getSort());
         List<Router> children = new ArrayList<>();
-        all.forEach(m->{
-            if (parent.getId().longValue()==m.getParentId().longValue()) {
-                children.add(getRouter(m,all));
+        all.forEach(m -> {
+            if (parent.getId().longValue() == m.getParentId().longValue()) {
+                children.add(getRouter(m, all));
             }
         });
         router.setChildren(children);
@@ -87,92 +90,133 @@ public class MenuService {
 
     /**
      * 菜单列表
+     *
      * @return
      */
     public List<Router> getMenuList(String title) {
-        List<Router> routers= new ArrayList<>();
+        List<Router> routers = new ArrayList<>();
         QueryWrapper<Menu> condition = new QueryWrapper<>();
         if (StringUtils.isNotBlank(title)) {
-            condition.apply(SqlHelper.getSqlLike(),SqlHelper.getSqlLikeParams(title));
+            condition.apply(SqlHelper.getSqlLike(), SqlHelper.getSqlLikeParams(title));
         }
         condition.orderByAsc("sort");
-        List<Menu> list= menuDao.selectList(condition);
+        List<Menu> list = menuDao.selectList(condition);
         //根节点
-        List<Menu> parents= list.stream().filter(m->m.getParentId()==null).collect(Collectors.toList());
+        List<Menu> parents = list.stream().filter(m -> m.getParentId() == null).collect(Collectors.toList());
         //子节点
-        List<Menu> child= list.stream().filter(m->m.getParentId()!=null).collect(Collectors.toList());
+        List<Menu> child = list.stream().filter(m -> m.getParentId() != null).collect(Collectors.toList());
         if (parents.isEmpty()) {
             return routers;
         }
-        parents.forEach(p->{
-            routers.add(getRouter(p,child));
+        parents.forEach(p -> {
+            routers.add(getRouter(p, child));
         });
         return routers;
     }
 
     /**
      * get zTree nodes
+     *
      * @return
      */
     public List<TreeNode> getMenuTree() {
 
-        List<TreeNode> nodes= new ArrayList<>();
+        List<TreeNode> nodes = new ArrayList<>();
         QueryWrapper<Menu> condition = new QueryWrapper<>();
         condition.orderByAsc("sort");
-        List<Menu> list= menuDao.selectList(condition);
+        List<Menu> list = menuDao.selectList(condition);
         //根节点
-        List<Menu> parents= list.stream().filter(m->m.getParentId()==null).collect(Collectors.toList());
+        List<Menu> parents = list.stream().filter(m -> m.getParentId() == null).collect(Collectors.toList());
         //子节点
-        List<Menu> child= list.stream().filter(m->m.getParentId()!=null).collect(Collectors.toList());
+        List<Menu> child = list.stream().filter(m -> m.getParentId() != null).collect(Collectors.toList());
         if (parents.isEmpty()) {
             return nodes;
         }
-        parents.forEach(p->{
-            nodes.add(getTreeNode(p,child));
+        parents.forEach(p -> {
+            nodes.add(getTreeNode(p, child));
         });
         return nodes;
     }
 
     public List<TreeNode> getMenuResTree() {
-        List<TreeNode> nodes= new ArrayList<>();
-        List<MenuResTree> tree= menuDao.selectMenuResTree();
+        List<TreeNode> nodes = new ArrayList<>();
+        List<MenuResTree> tree = menuDao.selectMenuResTree();
+        List<TreeNode> menuNodes = groupRes(tree);
         //根节点
-        List<MenuResTree> parents= tree.stream().filter(m->m.getParentId()==null).collect(Collectors.toList());
+        List<TreeNode> parents = menuNodes.stream().filter(m -> m.getParentId() == null).collect(Collectors.toList());
         //子节点
-        List<MenuResTree> child= tree.stream().filter(m->m.getParentId()!=null).collect(Collectors.toList());
+        List<TreeNode> child = menuNodes.stream().filter(m -> m.getParentId() != null).collect(Collectors.toList());
         if (parents.isEmpty()) {
             return nodes;
         }
-        parents.forEach(p->{
-            nodes.add(getTreeResNode(p,child));
+        parents.forEach(p -> {
+            nodes.add(getTreeResNode(p, child));
         });
         return nodes;
     }
 
-    private TreeNode getTreeResNode(MenuResTree parent, List<MenuResTree> all) {
-        TreeNode node= new TreeNode();
+    private List<TreeNode> groupRes(List<MenuResTree> res) {
+        long flag = -1;
+        List<TreeNode> treeNodes = new ArrayList<>();
+        TreeNode node = null;
+        for (MenuResTree tree : res) {
+            if (flag == tree.getId().longValue()) {
+                newResTreeNode(node,tree);
+            } else {
+                node = new TreeNode();
+                node.setName(tree.getName());
+                node.setParentId(tree.getParentId());
+                node.setId(tree.getId());
+                node.setChkDisabled(true);
+                if (tree.getResId() != null) {
+                    newResTreeNode(node,tree);
+                }
+                treeNodes.add(node);
+            }
+            flag = tree.getId();
+        }
+        return treeNodes;
+    }
+
+    private TreeNode newResTreeNode(TreeNode node,MenuResTree res) {
+        TreeNode resource = new TreeNode();
+        resource.setId(-res.getResId());
+        resource.setName(res.getResName());
+        resource.setParentId(res.getId());
+        node.getChildren().add(resource);
         return node;
     }
 
-    private TreeNode getTreeNode(Menu parent ,List<Menu> all) {
-        TreeNode node= new TreeNode();
+    private TreeNode getTreeResNode(TreeNode parent, List<TreeNode> all) {
+        List<TreeNode> children = parent.getChildren();
+        int index = 0;
+        for (TreeNode t : all) {
+            if (parent.getId().longValue() == t.getParentId().longValue()) {
+                children.add(index, getTreeResNode(t, all));
+                index++;
+            }
+        }
+        return parent;
+    }
+
+    private TreeNode getTreeNode(Menu parent, List<Menu> all) {
+        TreeNode node = new TreeNode();
         node.setName(parent.getTitle());
         node.setParentId(parent.getParentId());
         node.setId(parent.getId());
-        List<TreeNode> children = new ArrayList<>();
-        all.forEach(m->{
-            if (parent.getId().longValue()==m.getParentId().longValue()) {
-                children.add(getTreeNode(m,all));
+        List<TreeNode> children = node.getChildren();
+        all.forEach(m -> {
+            if (parent.getId().longValue() == m.getParentId().longValue()) {
+                children.add(getTreeNode(m, all));
             }
         });
-        node.setChildren(children);
         return node;
     }
 
     public void saveOrUpdateMenu(Router r) {
-        if (r.getId()!=null) {
-            Menu m= menuDao.selectById(r.getId());
-            if (m!=null) {
+        if (r.getId() != null) {
+            Menu m = menuDao.selectById(r.getId());
+            if (m != null) {
                 m.setCode(m.getCode());
                 m.setMeta(m.getMeta());
                 m.setSort(r.getSort());
@@ -185,8 +229,8 @@ public class MenuService {
                 menuDao.updateById(m);
             }
         } else {
-            Menu menu= new Menu();
-            BeanUtils.copyProperties(r,menu);
+            Menu menu = new Menu();
+            BeanUtils.copyProperties(r, menu);
             menu.setMeta(r.getMeta().toJSONString());
             menu.setCode(r.getName());
             menu.setCreateTime(new Date());
@@ -205,6 +249,6 @@ public class MenuService {
         res.setUrl(SqlHelper.getSqlLikeParams(res.getUrl()));
         res.setCode(SqlHelper.getSqlLikeParams(res.getCode()));
         res.setName(SqlHelper.getSqlLikeParams(res.getName()));
-        return resourceMapper.getMenuRes(page,res);
+        return resourceMapper.getMenuRes(page, res);
     }
 }
